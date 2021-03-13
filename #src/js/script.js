c51ecaf25c27623ee5eaa68a936f9bce7b7488c4
Stazzz <stazzzdzr@gmail.com>
2021-03-13 11:55:11 +0300

    4 block ready

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,5 +1,4 @@
-//полноэкранный скролл
-@@include('fullpage.js');
+@@include('fullpage.js');//полноэкранный скролл
 
 //функция для подключения webp
 function testWebP(callback) {
@@ -20,41 +19,105 @@ testWebP(function (support) {
 	}
 });
 
-//==================================
 
-
+//бургер и бургер меню
 window.addEventListener('DOMContentLoaded', () => {
 	const menu = document.querySelector('.navigation__flex'),
-	menuItem = document.querySelectorAll('.menu_item'),
-	burger = document.querySelector('.burger');
+		menuItem = document.querySelectorAll('.menu_item'),
+		burger = document.querySelector('.burger');
 
 
 	burger.addEventListener('click', () => {
-		 burger.classList.toggle('burger_active');
-		 menu.classList.toggle('navigation__flex_active');
+		burger.classList.toggle('burger_active');
+		menu.classList.toggle('navigation__flex_active');
 	});
 
 
 	menuItem.forEach(item => {
-		 item.addEventListener('click', () => {
-			  burger.classList.toggle('burger_active');
-			  menu.classList.toggle('navigation__flex_active');
-		 })
+		item.addEventListener('click', () => {
+			burger.classList.toggle('burger_active');
+			menu.classList.toggle('navigation__flex_active');
+		})
 	})
 });
 
 
 
-
+//fullpage scripts
 new fullpage('#fullpage', {
 	//options here
-	autoScrolling:true,
-	scrollHorizontally: true,
-	anchors:['firstPage', 'secondPage', 'thirdPage'],
-	navigation: true,
-	navigationTooltips: true,
-	navigationTooltips: ['Главная', 'О планете', 'Миссия'],
-	// showActiveTooltip: true,
-	responsiveWidth: 1000 //убераем прокрутку на определённом типе экрана
+	autoScrolling: true,//включает поэкранный скролл
+	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+	navigation: true,//буллиты навигации справа
+	navigationTooltips: ['Главная', 'О планете', 'Миссия', 'Наука'],
+	dragAndMove: true, //разрешает скролл на мобилах пальцем
+	responsiveWidth: 900, //убераем прокрутку на определённом типе экрана
+	verticalCentered: false, //Вертикальное центрирование контента в разделах
+	fitToSection: false //нужна ли подстройка разделов под окно просмотра.
 });
 
+
+
+//tabs
+var $tabs = function (target) {
+	var
+		_elemTabs = (typeof target === 'string' ? document.querySelector(target) : target),
+		_eventTabsShow,
+		_showTab = function (tabsLinkTarget) {
+			var tabsPaneTarget, tabsLinkActive, tabsPaneShow;
+			tabsPaneTarget = document.querySelector(tabsLinkTarget.getAttribute('href'));
+			tabsLinkActive = tabsLinkTarget.parentElement.querySelector('.tabs__link_active');
+			tabsPaneShow = tabsPaneTarget.parentElement.querySelector('.tabs__pane_show');
+			// если следующая вкладка равна активной, то завершаем работу
+			if (tabsLinkTarget === tabsLinkActive) {
+				return;
+			}
+			// удаляем классы у текущих активных элементов
+			if (tabsLinkActive !== null) {
+				tabsLinkActive.classList.remove('tabs__link_active');
+			}
+			if (tabsPaneShow !== null) {
+				tabsPaneShow.classList.remove('tabs__pane_show');
+			}
+			// добавляем классы к элементам (в завимости от выбранной вкладки)
+			tabsLinkTarget.classList.add('tabs__link_active');
+			tabsPaneTarget.classList.add('tabs__pane_show');
+			document.dispatchEvent(_eventTabsShow);
+		},
+		_switchTabTo = function (tabsLinkIndex) {
+			var tabsLinks = _elemTabs.querySelectorAll('.tabs__link');
+			if (tabsLinks.length > 0) {
+				if (tabsLinkIndex > tabsLinks.length) {
+					tabsLinkIndex = tabsLinks.length;
+				} else if (tabsLinkIndex < 1) {
+					tabsLinkIndex = 1;
+				}
+				_showTab(tabsLinks[tabsLinkIndex - 1]);
+			}
+		};
+
+	_eventTabsShow = new CustomEvent('tab.show', { detail: _elemTabs });
+
+	_elemTabs.addEventListener('click', function (e) {
+		var tabsLinkTarget = e.target;
+		// завершаем выполнение функции, если кликнули не по ссылке
+		if (!tabsLinkTarget.classList.contains('tabs__link')) {
+			return;
+		}
+		// отменяем стандартное действие
+		e.preventDefault();
+		_showTab(tabsLinkTarget);
+	});
+
+	return {
+		showTab: function (target) {
+			_showTab(target);
+		},
+		switchTabTo: function (index) {
+			_switchTabTo(index);
+		}
+	}
+
+};
+
+$tabs('.tabs');
